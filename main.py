e921bb485cc292537aae42c6dfd4a751dd210d59
snipe <moyalahow2005@gmail.com>
2025-07-08 00:45:06 -0400

    docs: update with comments for documentation

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
-
+# added get endpoint
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
+#another get endpoint added
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
+# new put endpoint added and edited
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
